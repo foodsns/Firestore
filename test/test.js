2262c845e8a6afe58357313e2bf442847bf9261d
Kim HyunWoo <stories2@naver.com>
2021-10-05 16:45:06 +0900

    Update test.js

--- test/test.js
+++ test/test.js
@@ -3,6 +3,13 @@ const firebase = require('@firebase/testing')
 
 const MY_PROJECT_ID = 'mogutsou'
 
+const db = firebase.initializeTestApp({
+    projectId: MY_PROJECT_ID,
+}).firestore()
+db.settings({
+    host: "localhost:8081",
+    ssl: false
+});
 
 describe("Our social app", () => {
     it("Understands basic addtion", () => {
@@ -10,25 +17,11 @@ describe("Our social app", () => {
     })
 
     it ("Can read items in the read-only collection", async () => {
-        const db = firebase.initializeTestApp({
-            projectId: MY_PROJECT_ID,
-        }).firestore()
-        db.settings({
-            host: "localhost:8081",
-            ssl: false
-        });
         const testDoc = db.collection("readonly").doc("testDoc")
         await firebase.assertSucceeds(testDoc.get());
     })
 
     it ("Can't write to items in the read-only collection", async () => {
-        const db = firebase.initializeTestApp({
-            projectId: MY_PROJECT_ID,
-        }).firestore()
-        db.settings({
-            host: "localhost:8081",
-            ssl: false
-        });
         const testDoc = db.collection("readonly").doc("testDoc")
         await firebase.assertFails(testDoc.set({foo: "bar"}));
     })
