f343f0a4b0bcdb92adea962234251d9419ceabf8
Kim HyunWoo <stories2@naver.com>
2021-10-05 18:35:57 +0900

    Refactoring testcases

--- test/test.js
+++ test/test.js
@@ -2,14 +2,25 @@ const assert = require('assert')
 const firebase = require('@firebase/testing')
 
 const MY_PROJECT_ID = 'mogutsou'
+const myId = "user_abc"
+const theirId = "user_xyz"
+const myAuth = {uid: myId, email: 'abc@example.com'}
 
-const db = firebase.initializeTestApp({
-    projectId: MY_PROJECT_ID,
-}).firestore()
-db.settings({
-    host: "localhost:8081",
-    ssl: false
-});
+function getFirebase(auth) {
+    return firebase.initializeTestApp({
+        projectId: MY_PROJECT_ID,
+        auth
+    }).firestore()
+}
+
+function getFirestore(auth) {
+    const db = getFirebase(auth)
+    db.settings({
+        host: "localhost:8081",
+        ssl: false
+    });
+    return db;
+}
 
 describe("Our social app", () => {
     it("Understands basic addtion", () => {
@@ -17,40 +28,23 @@ describe("Our social app", () => {
     })
 
     it ("Can read items in the read-only collection", async () => {
-        const testDoc = db.collection("readonly").doc("testDoc")
+        const testDoc = getFirestore().collection("readonly").doc("testDoc")
         await firebase.assertSucceeds(testDoc.get());
     })
 
     it ("Can't write to items in the read-only collection", async () => {
-        const testDoc = db.collection("readonly").doc("testDoc")
+        const testDoc = getFirestore().collection("readonly").doc("testDoc")
         await firebase.assertFails(testDoc.set({foo: "bar"}));
     })
 
     it ("Can write to a user document with the same ID as our user", async () => {
-        const myAuth = {uid: 'user_abc', email: 'abc@example.com'}
-        const dbAuth = firebase.initializeTestApp({
-            projectId: MY_PROJECT_ID,
-            auth: myAuth
-        }).firestore()
-        dbAuth.settings({
-            host: "localhost:8081",
-            ssl: false
-        });
-        const testDoc = dbAuth.collection("users").doc("user_abc")
+        const testDoc = getFirestore(myAuth).collection("users").doc("user_abc")
         await firebase.assertSucceeds(testDoc.set({foo: "bar"}))
     })
 
     it ("Can't write to a user document with a different ID as our user", async () => {
-        const myAuth = {uid: 'user_abc', email: 'abc@example.com'}
-        const dbAuth = firebase.initializeTestApp({
-            projectId: MY_PROJECT_ID,
-            auth: myAuth
-        }).firestore()
-        dbAuth.settings({
-            host: "localhost:8081",
-            ssl: false
-        });
-        const testDoc = dbAuth.collection("users").doc("user_xyz")
+        myAuth.uid = theirId
+        const testDoc = getFirestore(myAuth).collection("users").doc("user_xyz")
         await firebase.assertFails(testDoc.set({foo: "bar"}))
     })
 })
