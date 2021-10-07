ee117e0b171cc8e6d896aafd91f0a77dec615f96
Kim HyunWoo <stories2@naver.com>
2021-10-08 00:50:03 +0900

    Add testcase. Get list of posts which is marked as good by me

--- test/good.test.js
+++ test/good.test.js
@@ -64,6 +64,36 @@ describe("Test goods collection which is post's subcollection", () => {
                 hashtag: '러시아공사관'
             })
         await firebase.assertSucceeds(getFirestore(myAuth).collection("posts").doc(postID).collection("goods").doc(myAuth.uid).set({
+            authorId: myAuth.uid,
+            setTime: firebase.firestore.FieldValue.serverTimestamp()
+        }))
+    })
+    it("Can set good mark to other post2", async () => {
+        const myAuth = {uid: myId, email: 'abc@example.com'}
+        const postID = "post_good_ghi"
+        const setQuery = getFirestore(null, true).collection("posts").doc(postID)
+        await setQuery.set(
+            {
+                id: 'ae3f053e-e0d4-486b-af6e-3d6138d426f9',
+                title: '구 러시아공사관',
+                descript: '구 러시아공사관',
+                date: '2021년 9월 4일',
+                profileImg: 'https://avatars.githubusercontent.com/u/16532326?v=4',
+                writer: 'stories2stories2stories2stories2stories2stories2stories2stories2stories2',
+                good: 8,
+                img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Former_Russia_legation_of_Korea_01.JPG/272px-Former_Russia_legation_of_Korea_01.JPG',
+                lat: 37.568178,
+                lot: 126.971474,
+                visibility: "public",
+                authorId: theirId,
+                country: 'Korea', 
+                city: '서울특별시',
+                state: '중구',
+                street: '정동',
+                hashtag: '러시아공사관'
+            })
+        await firebase.assertSucceeds(getFirestore(myAuth).collection("posts").doc(postID).collection("goods").doc(myAuth.uid).set({
+            authorId: myAuth.uid,
             setTime: firebase.firestore.FieldValue.serverTimestamp()
         }))
     })
@@ -92,13 +122,22 @@ describe("Test goods collection which is post's subcollection", () => {
                 hashtag: '러시아공사관'
             })
         await firebase.assertFails(getFirestore(myAuth).collection("posts").doc(postID).collection("goods").doc(myAuth.uid).set({
+            authorId: myAuth.uid,
             setTime: firebase.firestore.FieldValue.serverTimestamp()
         }))
     })
     it("Can't set good mark to my post who not logged in", async () => {
         const postID = "post_good_def"
         await firebase.assertFails(getFirestore().collection("posts").doc(postID).collection("goods").doc("anonymous").set({
+            authorId: "anonymous",
             setTime: firebase.firestore.FieldValue.serverTimestamp()
         }))
+    })
+
+    it("Get my goods list", async () => {
+        const myAuth = {uid: myId, email: 'abc@example.com'}
+        const queryResult = await Promise.allSettled((await getFirestore(myAuth).collectionGroup('goods').where("authorId", "==", `${myAuth.uid}`).get()).docs.map(async (item) => await ((await item.ref.parent.parent.get()).data())))
+        await firebase.assertSucceeds(queryResult.length == 2)
+        await firebase.assertSucceeds(queryResult.filter(item => item.status.fulfilled).length == 1)
     })
 })
